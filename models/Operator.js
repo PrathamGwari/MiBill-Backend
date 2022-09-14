6d3e16563cae4d00508999a0d2707bbcb3ca5549
unknown <prathamgwari.pg@gmail.com>
2022-09-14 11:33:24 +0530

    --Added new schemas

--- models/Operator.js
+++ models/Operator.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const UserSchema = mongoose.Schema({
+const OperatorSchema = mongoose.Schema({
     username: {
         type: String,
         required: true
@@ -16,4 +16,4 @@ const UserSchema = mongoose.Schema({
     }
 })
 
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model('Operator', OperatorSchema)
